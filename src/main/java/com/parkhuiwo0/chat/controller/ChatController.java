ff0c400a89bea9c408f11870be9293055bd84487
parkhuiwo0 <huiwo0@naver.com>
2020-11-30 15:33:42 +0900

    ChatController 추가

---
+++ src/main/java/com/parkhuiwo0/chat/controller/ChatController.java
@@ -0,0 +1,26 @@
+package com.parkhuiwo0.chat.controller;
+
+import com.parkhuiwo0.chat.ChatService;
+import com.parkhuiwo0.chat.domain.ChatRoom;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/chat")
+public class ChatController {
+
+    private final ChatService chatService;
+
+    @PostMapping
+    public ChatRoom createRoom(@RequestParam("name") String name) {
+        return chatService.createRoom(name);
+    }
+
+    @GetMapping
+    public List<ChatRoom> findAllRoom() {
+        return chatService.findAllRoom();
+    }
+}
