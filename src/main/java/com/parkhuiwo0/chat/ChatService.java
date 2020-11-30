b16d0464effa1bc6ec2226d1e9c2993f4282a54f
parkhuiwo0 <huiwo0@naver.com>
2020-11-30 15:24:52 +0900

    ChatService 추가

---
+++ src/main/java/com/parkhuiwo0/chat/ChatService.java
@@ -0,0 +1,56 @@
+package com.parkhuiwo0.chat;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.parkhuiwo0.chat.domain.ChatRoom;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class ChatService {
+
+    private final ObjectMapper objectMapper;
+    private Map<String, ChatRoom> chatRooms;
+
+    @PostConstruct
+    private void init() {
+        chatRooms = new LinkedHashMap<>();
+    }
+
+    public List<ChatRoom> findAllRoom() {
+        return new ArrayList<>(chatRooms.values());
+    }
+
+    public ChatRoom findRoomById(String roomId) {
+        return chatRooms.get(roomId);
+    }
+
+    public ChatRoom createRoom(String name) {
+        String id = UUID.randomUUID().toString();
+        ChatRoom chatRoom = ChatRoom.builder()
+                .roomId(id)
+                .name(name)
+                .build();
+
+        chatRooms.put(id, chatRoom);
+
+        return chatRoom;
+    }
+
+    public <T> void sendMessage(WebSocketSession session, T message) {
+        try {
+            session.sendMessage(new TextMessage(objectMapper.writeValueAsString(message)));
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+}
