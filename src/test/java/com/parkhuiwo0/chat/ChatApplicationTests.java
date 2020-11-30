5e2a51e8fc85a769c64013283080653f0caa6a67
parkhuiwo0 <huiwo0@naver.com>
2020-11-30 13:34:09 +0900

    initialize prject

---
+++ src/test/java/com/parkhuiwo0/chat/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkhuiwo0.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
