b8aff5a6e79ed2c8665de0a53eeb5c13d44aa237
parkhuiwo0 <huiwo0@naver.com>
2020-12-01 17:44:52 +0900

    refactor : ChatController Publisher 구현

--- src/main/java/com/parkhuiwo0/chat/controller/ChatController.java
+++ src/main/java/com/parkhuiwo0/chat/controller/ChatController.java
@@ -1,8 +1,11 @@
 package com.parkhuiwo0.chat.controller;
 
+import com.parkhuiwo0.chat.domain.ChatMessage;
 import com.parkhuiwo0.chat.service.ChatService;
 import com.parkhuiwo0.chat.domain.ChatRoom;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,15 +15,26 @@ import java.util.List;
 @RequestMapping("/chat")
 public class ChatController {
 
-    private final ChatService chatService;
+//    private final ChatService chatService;
 
-    @PostMapping
-    public ChatRoom createRoom(@RequestParam("name") String name) {
-        return chatService.createRoom(name);
+    private final SimpMessageSendingOperations messagingTemplate;
+
+    @MessageMapping("/chat/message")
+    public void message(ChatMessage message) {
+        if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
+            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+        }
+        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
     }
 
-    @GetMapping
-    public List<ChatRoom> findAllRoom() {
-        return chatService.findAllRoom();
-    }
+
+//    @PostMapping
+//    public ChatRoom createRoom(@RequestParam("name") String name) {
+//        return chatService.createRoom(name);
+//    }
+//
+//    @GetMapping
+//    public List<ChatRoom> findAllRoom() {
+//        return chatService.findAllRoom();
+//    }
 }
