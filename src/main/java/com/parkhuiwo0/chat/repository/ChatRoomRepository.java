9f6158e507777410384a219df44f54a5698eed0e
parkhuiwo0 <huiwo0@naver.com>
2020-12-01 17:39:01 +0900

    feat : ChatRoomRepository 추가 (pub/sub)

---
+++ src/main/java/com/parkhuiwo0/chat/repository/ChatRoomRepository.java
@@ -0,0 +1,35 @@
+package com.parkhuiwo0.chat.repository;
+
+import com.parkhuiwo0.chat.domain.ChatRoom;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import java.util.*;
+
+@Repository
+public class ChatRoomRepository {
+
+    private Map<String, ChatRoom> chatRoomMap;
+
+    @PostConstruct
+    public void init() {
+        chatRoomMap = new LinkedHashMap<>();
+    }
+
+    public List<ChatRoom> findAllRoom() {
+        // 채팅방 생성순서 최근 순으로 반환
+        List chatRooms = new ArrayList<>(chatRoomMap.values());
+        Collections.reverse(chatRooms);
+        return chatRooms;
+    }
+
+    public ChatRoom findRoomById(String id) {
+        return chatRoomMap.get(id);
+    }
+
+    public ChatRoom createChatRoom(String name) {
+        ChatRoom chatRoom = ChatRoom.create(name);
+        chatRoomMap.put(chatRoom.getRoomId(), chatRoom);
+        return chatRoom;
+    }
+}
