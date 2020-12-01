9e016dd230cb11d91234dcfe3377deac1f36c53f
parkhuiwo0 <huiwo0@naver.com>
2020-12-01 17:37:11 +0900

    refactor : ChatRoom 도메인 수정(pub/sub 모델)

--- src/main/java/com/parkhuiwo0/chat/common/WebSocketConfig.java
+++ src/main/java/com/parkhuiwo0/chat/common/WebSocketConfig.java
@@ -2,20 +2,32 @@ package com.parkhuiwo0.chat.common;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final WebSocketChatHandler webSocketChatHandler;
 
+//    @Override
+//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+//        // setAllowedOrigins -> CORS 이슈 해결
+//        registry.addHandler(webSocketChatHandler, "/ws/chat").setAllowedOrigins("*");
+//    }
+
+
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        // setAllowedOrigins -> CORS 이슈 해결
-        registry.addHandler(webSocketChatHandler, "/ws/chat").setAllowedOrigins("*");
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub");
+        registry.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-stomp").setAllowedOrigins("*")
+                .withSockJS();
     }
 }
--- src/main/java/com/parkhuiwo0/chat/domain/ChatRoom.java
+++ src/main/java/com/parkhuiwo0/chat/domain/ChatRoom.java
@@ -7,29 +7,38 @@ import org.springframework.web.socket.WebSocketSession;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
 @Getter
 public class ChatRoom {
     private String roomId;
     private String name;
-    private Set<WebSocketSession> sessions = new HashSet<>();
+//    private Set<WebSocketSession> sessions = new HashSet<>(); // pub/sub의 경우 구독자 관리가 알아서 되기 때문에
 
-    @Builder
-    public ChatRoom(String roomId, String name) {
-        this.roomId = roomId;
-        this.name = name;
+    public static ChatRoom create(String name) {
+        ChatRoom chatRoom = new ChatRoom();
+        chatRoom.roomId = UUID.randomUUID().toString();
+        chatRoom.name = name;
+
+        return chatRoom;
     }
 
-    public void handleActions(WebSocketSession session, ChatMessage chatMessage, ChatService chatService) {
-//        if (chatMessage.getType().equals(MessageType.ENTER)) {
-        if (chatMessage.getType().equals(ChatMessage.MessageType.ENTER)) {
-            sessions.add(session);
-            chatMessage.setMessage(chatMessage.getSender() + "님이 입장했습니다.");
-        }
-        sendMessage(chatMessage, chatService);
-    }
+//    @Builder
+//    public ChatRoom(String roomId, String name) {
+//        this.roomId = roomId;
+//        this.name = name;
+//    }
 
-    public <T> void sendMessage(T message, ChatService chatService) {
-        sessions.parallelStream().forEach(session -> chatService.sendMessage(session, message));
-    }
+//    public void handleActions(WebSocketSession session, ChatMessage chatMessage, ChatService chatService) {
+////        if (chatMessage.getType().equals(MessageType.ENTER)) {
+//        if (chatMessage.getType().equals(ChatMessage.MessageType.ENTER)) {
+//            sessions.add(session);
+//            chatMessage.setMessage(chatMessage.getSender() + "님이 입장했습니다.");
+//        }
+//        sendMessage(chatMessage, chatService);
+//    }
+//
+//    public <T> void sendMessage(T message, ChatService chatService) {
+//        sessions.parallelStream().forEach(session -> chatService.sendMessage(session, message));
+//    }
 }
