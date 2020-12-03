e09b4b39b94c04cc64871bfd7797ad7b243987fa
parkhuiwo0 <huiwo0@naver.com>
2020-12-03 11:24:37 +0900

    refactor : ChatRoomRepository Redis 적용

--- src/main/java/com/parkhuiwo0/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/parkhuiwo0/chat/repository/ChatRoomRepository.java
@@ -1,35 +1,72 @@
 package com.parkhuiwo0.chat.repository;
 
+import com.parkhuiwo0.chat.common.RedisSubscriber;
 import com.parkhuiwo0.chat.domain.ChatRoom;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
 import java.util.*;
 
 @Repository
+@RequiredArgsConstructor
 public class ChatRoomRepository {
 
-    private Map<String, ChatRoom> chatRoomMap;
+    private final RedisMessageListenerContainer redisMessageListenerContainer;
+    private final RedisSubscriber redisSubscriber;
+    private static final String CHAT_ROOMS = "CHAT_ROOM";
+    private final RedisTemplate<String, Object> redisTemplate;
+    private HashOperations<String, String, ChatRoom> opsHashChatRoom;
+    private Map<String, ChannelTopic> topics;
+
+//    private Map<String, ChatRoom> chatRoomMap;
+
+//    @PostConstruct
+//    public void init() {
+//        chatRoomMap = new LinkedHashMap<>();
+//    }
 
     @PostConstruct
     public void init() {
-        chatRoomMap = new LinkedHashMap<>();
+        opsHashChatRoom = redisTemplate.opsForHash();
+        topics = new HashMap<>();
     }
 
     public List<ChatRoom> findAllRoom() {
-        // 채팅방 생성순서 최근 순으로 반환
-        List chatRooms = new ArrayList<>(chatRoomMap.values());
-        Collections.reverse(chatRooms);
-        return chatRooms;
+//        // 채팅방 생성순서 최근 순으로 반환
+//        List chatRooms = new ArrayList<>(chatRoomMap.values());
+//        Collections.reverse(chatRooms);
+//        return chatRooms;
+
+        return opsHashChatRoom.values(CHAT_ROOMS);
     }
 
     public ChatRoom findRoomById(String id) {
-        return chatRoomMap.get(id);
+        return opsHashChatRoom.get(CHAT_ROOMS, id);
     }
 
     public ChatRoom createChatRoom(String name) {
         ChatRoom chatRoom = ChatRoom.create(name);
-        chatRoomMap.put(chatRoom.getRoomId(), chatRoom);
+//        chatRoomMap.put(chatRoom.getRoomId(), chatRoom);
+        opsHashChatRoom.put(CHAT_ROOMS, chatRoom.getRoomId(), chatRoom);
+
         return chatRoom;
+    }
+
+    public void enterChatRoom(String roomId) {
+        ChannelTopic topic = topics.get(roomId);
+        if (topic == null) {
+            topic = new ChannelTopic(roomId);
+            redisMessageListenerContainer.addMessageListener(redisSubscriber, topic);
+            topics.put(roomId, topic);
+        }
+    }
+
+    public ChannelTopic getTopic(String roomId) {
+        return topics.get(roomId);
     }
 }
